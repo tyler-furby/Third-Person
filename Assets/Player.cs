134ffc531377198c02e4aad40776c7b399bf5548
Tyler Furby <tyler@tylerfurby.com>
2017-07-24 16:03:13 -0600

    Implement rough third person camera

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float turningSpeed = 60;
+	public float movementSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+		transform.Rotate(0, horizontal, 0);
+
+		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		transform.Translate(0, 0, vertical);
+	}
+}
