44c608924ed5f9b80be2c8f7549861ced554a9d6
Tyler Furby <tyler@tylerfurby.com>
2017-07-24 18:01:34 -0600

    Fixed player's gun position

---
+++ Assets/Scripts/MCSBlendshapeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MORPH3D;
+
+public class MCSBlendshapeController : MonoBehaviour {
+
+	private int count;
+	private M3DCharacterManager m_CharacterManager;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		m_CharacterManager = GetComponent<M3DCharacterManager> ();
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag("Wall")) {
+			count++;
+			m_CharacterManager.SetBlendshapeValue ("FBMHeavy", count * (100.0f / 12));
+	}
+}
+
+}
