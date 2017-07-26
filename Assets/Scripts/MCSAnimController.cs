fab286e9d14e713d8068b4dfb939b4133f5a2510
Tyler Furby <tyler@tylerfurby.com>
2017-07-26 00:41:42 -0600

    Recreated player, added backwards movement animation

--- Assets/Scripts/MCSAnimController.cs
+++ Assets/Scripts/MCSAnimController.cs
@@ -7,14 +7,19 @@ public class MCSAnimController : MonoBehaviour {
 	private Animator anim;
 	private float walking;
 	private float turning;
+	private float walkingback;
 
 	public int turnSpeed;
+
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		walking = 0.0f;
+		walkingback = 0.0f;
 		turning = 0.0f;
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -24,5 +29,9 @@ public class MCSAnimController : MonoBehaviour {
 
 		turning = Input.GetAxis ("Horizontal");
 		transform.Rotate (new Vector3 (0.0f, turnSpeed * turning * Time.deltaTime));
-	}
+
+		walkingback = Input.GetAxis ("Vertical");
+		anim.SetFloat ("walkingback", walkingback);
+}
+
 }
